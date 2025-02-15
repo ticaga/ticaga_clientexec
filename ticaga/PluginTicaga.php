1c5b05faf79a2db1ba9ce11e4bbe4edd20bdfbab
Michael Dance <119854202+Ticaga-Mike@users.noreply.github.com>
2025-02-15 15:31:04 +0000

    Added Variables

--- ticaga/PluginTicaga.php
+++ ticaga/PluginTicaga.php
@@ -9,5 +9,16 @@ require_once 'modules/admin/models/SnapinPlugin.php';
 
 class PluginTicaga extends SnapinPlugin
 {
+    public function getVariables()
+{
+    $variables = [
+        'Plugin Name' => [
+            'type' => 'hidden',
+            'description' => 'Used by CE to show plugin',
+            'value' => 'Ticaga'
+        ]
+    ];
 
+    return $variables;
+}
 }
