3ed7f3ee9877730666b009667ae326c4f323824c
Michael Dance <119854202+Ticaga-Mike@users.noreply.github.com>
2025-02-15 15:42:32 +0000

    Addded required functions.

--- ticaga/PluginTicaga.php
+++ ticaga/PluginTicaga.php
@@ -9,32 +9,40 @@ require_once 'modules/admin/models/SnapinPlugin.php';
 
 class PluginTicaga extends SnapinPlugin
 {
+    public init()
+    {
+        /* 
+         * Each snapin should have an init() function, 
+         * which will call internal functions to determine what the snapin does. 
+         */
+    }
+
     public $listeners = [
         ["Client-Create", "Client-Update", "Client-PasswordChange"]
     ];
 
     /*
-    * Client-Create
-    * When a client is created on ClientExec, create the user on Ticaga.
-    */
+     * Client-Create
+     * When a client is created on ClientExec, create the user on Ticaga.
+     */
     public function Client-Create($e)
     {
         return false;
     }
 
     /*
-    * Client-Update
-    * When a client is updated on ClientExec, update the user on Ticaga.
-    */
+     * Client-Update
+     * When a client is updated on ClientExec, update the user on Ticaga.
+     */
     public function Client-Update($e)
     {
         return false;
     }
 
     /*
-    * Client-PasswordChange
-    * When a client password is changed on ClientExec, send a reset password to the user on Ticaga.
-    */
+     * Client-PasswordChange
+     * When a client password is changed on ClientExec, send a reset password to the user on Ticaga.
+     */
     public function Client-PasswordChange($e)
     {
         return false;
