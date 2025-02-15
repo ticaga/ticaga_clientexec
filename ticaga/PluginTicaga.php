ebf1163ed8b85ec407110e1396e3d849884d072b
Michael Dance <119854202+Ticaga-Mike@users.noreply.github.com>
2025-02-15 15:28:20 +0000

    Basic set-up

---
+++ ticaga/PluginTicaga.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Ticaga ClientExec plugin
+ *
+ * @link https://ticaga.com/ Ticaga
+ */
+
+require_once 'modules/admin/models/SnapinPlugin.php';
+
+class PluginTicaga extends SnapinPlugin
+{
+
+}
